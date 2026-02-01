89062311ae85b633f79a9408f54d4717234d7747
iamkashifyousuf <kashifyousuf.sc@gmail.com>
2026-02-01 17:17:06 +0500

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,119 @@
+use crate::EntryType::*;
+use clap::{App, Arg};
+use regex::Regex;
+use std::error::Error;
+use walkdir::WalkDir;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug, Eq, PartialEq)]
+enum EntryType {
+    Dir,
+    File,
+    Link,
+}
+
+#[derive(Debug)]
+pub struct Config {
+    paths: Vec<String>,
+    names: Vec<Regex>,
+    // names: Vec<String>,
+    entry_types: Vec<EntryType>,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("findr")
+        .version("0.1.0")
+        .author("Kashif Yousuf <kashifyousuf.sc@gmail.com>")
+        .about("find in Rust")
+        .arg(
+            Arg::with_name("paths")
+                .help("Search Path(s)")
+                .value_name("PATH")
+                .multiple(true)
+                .default_value("."),
+        )
+        .arg(
+            Arg::with_name("names")
+                .help("NAME")
+                .value_name("NAME")
+                .short("n")
+                .long("name")
+                .takes_value(true)
+                .multiple(true),
+        )
+        .arg(
+            Arg::with_name("entry_types")
+                .help("entry_types")
+                .value_name("Entry Types")
+                .short("t")
+                .long("type")
+                .takes_value(true)
+                .multiple(true)
+                .possible_values(&["d", "f", "l"]),
+        )
+        .get_matches();
+
+    let entry_types = matches
+        .values_of_lossy("entry_types")
+        .map(|types| {
+            types
+                .iter()
+                .map(|t| match t.as_str() {
+                    "d" => Dir,
+                    "f" => File,
+                    "l" => Link,
+                    _ => unreachable!("Not found"),
+                })
+                .collect()
+        })
+        .unwrap_or_default();
+
+    let names = matches
+        .values_of_lossy("names")
+        .map(|names| {
+            names
+                .into_iter()
+                .map(|name| Regex::new(&name).map_err(|_| format!("Invalid --name \"{}\"", name)))
+                .collect::<Result<Vec<_>, _>>()
+        })
+        .transpose()?
+        .unwrap_or_default();
+
+    Ok(Config {
+        paths: matches.values_of_lossy("paths").unwrap(),
+        names,
+        entry_types,
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    for path in config.paths {
+        for entry in WalkDir::new(path) {
+            match entry {
+                Err(e) => eprintln!("{}", e),
+                Ok(entry) => {
+                    if (config.entry_types.is_empty()
+                        || config
+                            .entry_types
+                            .iter()
+                            .any(|entry_type| match entry_type {
+                                Link => entry.file_type().is_symlink(),
+                                Dir => entry.file_type().is_dir(),
+                                File => entry.file_type().is_file(),
+                            }))
+                        && (config.names.is_empty()
+                            || config
+                                .names
+                                .iter()
+                                .any(|re| re.is_match(&entry.file_name().to_string_lossy())))
+                    {
+                        println!("{}", entry.path().display());
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
